7e2598248d60ce0f202ea2af9f3802959c9284d5
Genc Arifaj <genci.arifaj1@gmail.com>
2023-07-12 10:43:20 +0200

    protec shop

---
+++ src/pages/contact.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Contact = () => {
+  return <div>you can contact us at: protec.shop@gmail.com</div>;
+};
---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/ip14.jpg";
+import product2 from "./assets/ip13.jpg";
+import product3 from "./assets/ip12.jpg";
+import product4 from "./assets/mc1.jpg";
+import product5 from "./assets/mc2.jpg";
+import product6 from "./assets/mc1.jpg";
+import product7 from "./assets/s1.jpg";
+import product8 from "./assets/s1.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "iPhone 14",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "iPhone 13",
+    price: 799.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "iPhone 12",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Macbook Pro M2",
+    price: 1999.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Macbook Air M2",
+    price: 1599.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Macbook Pro M1",
+    price: 1099.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Samsung Galaxy S22 ultra",
+    price: 1199.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Samsung Galaxy S21",
+    price: 899.0,
+    productImage: product8,
+  },
+];
